3c8f3d8c68e1f7a1bdc71ca69817a484496df382
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-01-28 20:24:38 -0500

    added Manager object with properties, methods, and tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,32 +5,35 @@ test('Can create an instance of employee object', () => {
     expect(typeof(e)).toBe('object');
 });
 
-test('Can set the employee name through constructor', () => {
+test('Can set the employee name through constructor, passed as a string', () => {
     const name = 'Bob';
     const e = new Employee(name);
     expect(e.name).toBe(name);
+    expect(e.name).toEqual(expect.any(String));
 });
 
-test('Can set the employee id through constructor', () => {
+test('Can set the employee id through constructor, passed as a number', () => {
     const name = 'Bob';
-    const id = '12345';
+    const id = 12345;
     const e = new Employee(name, id)
     expect(e.id).toBe(id);
+    expect(e.id).toEqual(expect.any(Number))
 
 });
 
 test('Can set the employee id through constructor', () => {
     const name = 'Bob';
-    const id = '12345';
+    const id = 12345;
     const email = 'bob@company.com'
     const e = new Employee(name, id, email)
     expect(e.email).toBe(email);
+    expect(e.email).toEqual(expect.any(String));
 
 });
 
 test('Can access employee name through getName()', () => {
     const name = 'Bob';
-    const id = '12345';
+    const id = 12345;
     const email = 'bob@company.com'
     const e = new Employee(name, id, email)
     expect(e.getName()).toBe(name);
@@ -38,7 +41,7 @@ test('Can access employee name through getName()', () => {
 
 test('Can access employee id through getId()', () => {
     const name = 'Bob';
-    const id = '12345';
+    const id = 12345;
     const email = 'bob@company.com'
     const e = new Employee(name, id, email)
     expect(e.getId()).toBe(id);
@@ -46,7 +49,7 @@ test('Can access employee id through getId()', () => {
 
 test('Can access employee email through getEmail()', () => {
     const name = 'Bob';
-    const id = '12345';
+    const id = 12345;
     const email = 'bob@company.com'
     const e = new Employee(name, id, email)
     expect(e.getEmail()).toBe(email);
@@ -54,7 +57,7 @@ test('Can access employee email through getEmail()', () => {
 
 test('Can access employee role through getRole()', () => {
     const name = 'Bob';
-    const id = '12345';
+    const id = 12345;
     const email = 'bob@company.com'
     const e = new Employee(name, id, email)
     expect(e.getRole()).toBe('Employee');
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,39 @@
+const Manager = require('../lib/Manager');
+
+test('Can create an instance of manager object', () => {
+    const m = new Manager();
+    expect(typeof(m)).toBe('object');
+});
+
+// test("Can set a manager object's properties through the constructor", () => {
+//     const name = 'Steve';
+//     const id = 67890;
+//     const email = 'steve@company.com'
+//     const officeNumber = 222;
+//     const m = new Manager(name, id, email, officeNumber);
+//     expect(m.name).toBe(name);
+//     expect(m.name).toEqual(expect.any(String));
+//     expect(m.id).toBe(id);
+//     expect(m.id).toEqual(expect.any(Number));
+//     expect(m.email).toBe(email);
+//     expect(m.email).toEqual(expect.any(String));
+//     expect(m.officeNumber).toBe(officeNumber);
+//     expect(m.officeNumber).toEqual(expect.any(Number));
+
+// });
+
+test("Can set a manager's office number through the constructor", () => {
+    const officeNumber = 222;
+    const m = new Manager('Steve', 67890, 'steve@company.com', officeNumber);
+
+    expect(m.officeNumber).toBe(officeNumber);
+    expect(m.officeNumber).toEqual(expect.any(Number));
+
+});
+
+test("Can see a manager's role through getRole()", () => {
+    const m = new Manager('Steve', 67890, 'steve@company.com', 222);
+
+    expect(m.getRole()).toBe('Manager')
+
+});
