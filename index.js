ad2bd10cc4f1c775ec5066f0edd15f643c4879b6
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-01-29 17:04:06 -0500

    added prompts for the engineer and intern object, and both of these and the manager questions seperate modules

--- index.js
+++ index.js
@@ -6,76 +6,14 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const managerQuestions = require('./lib/managerQuestions');
+const engineerQuestions = require('./lib/engineerQuestions');
+const internQuestions = require('./lib/internQuestions');
+
 
 const employees = [];
 
-const managerQuestions = [
-    {
-        type: 'input',
-        name: 'managerName', 
-        message: 'Enter the name of the Team Manager:',
-        validate: (managerNameInput) => {
-            if (managerNameInput) {
-                return true;
-            } else {
-                console.log('Please enter the name of the Team Manager')
-                return false; 
-            }
-        },
-    },
-    {
-        type: 'number',
-        name: 'managerId',
-        message: 'Enter the id number of the manager:',
-        validate: (managerIdInput) => {
-            if (managerIdInput) {
-                return true;
-            } else {
-                console.log('Please enter the id number of the manager')
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'Enter the email address of the manager:',
-        validate: (managerEmailInput) => {
-            if (managerEmailInput) {
-                return true;
-            } else {
-                console.log('Please enter the email address of the manager')
-                return false;
-            }
-        }
-    },
-    {
-        type: 'number',
-        name: 'managerOffice',
-        message: 'Enter the office number of the manager:',
-        validate: (managerOfficeInput) => {
-            if (managerOfficeInput) {
-                return true;
-            } else {
-                console.log('Please enter the office number of the manager')
-                return false;
-            }
-        }
-    },
-    // {
-    //     type: 'list',
-    //     name: 'toDoNext',
-    //     message: 'What would you like to do next?',
-    //     choices: ['Add an engineer', 'Add an intern', 'Finish']
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'engineerGithub'
-    //     message: enter 
-    // }
-    
 
-];
 
 
 
@@ -93,9 +31,9 @@ function inputNextChoice () {
     ])
     .then((answer) => {
         if (answer.choice ==='Add an engineer'){
-            promptUser(engineerQuestions)
-        } else {
-            console.log('here');
+            promptUser(engineerQuestions);   
+        } else if (answer.choice ==='Add an intern'){
+            promptUser(internQuestions);
         }
 
     })
