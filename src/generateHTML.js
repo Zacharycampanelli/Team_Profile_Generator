e86605a82220e4ac72d1b6e5ca634aa59c1f435d
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-02-02 14:36:27 -0500

    added function to display a card on html page:

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,16 +1,39 @@
-function generateTeamMember(data) {
-  for (let i = 0; i < data.length; i++) {
-    console.log("this" + data[i])
-  }
+function generateManager(data) {
+
+  
 }
 
-function generateHTML(data) {
+function buildCard(data) {
+
+    // let role = data[0].getRole();
+    // if (role === "Manager") {
+    //   generateManager(data[0])
+    // } else if (role === "Engineer") {
+
+    // } else if (role === "Intern") {
+
+    // }
+    return  `
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+      Featured
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">An item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+    </ul>
+   </div>
+    `
+
+  }
+
+
+  module.exports = (data) => {
     // const {name, ...header} = data
-    for (let i = 0; i < data.length; i++) {
-      //const employee = data[i];
-      console.log("hi" + data[i].name);}
+    
   
-    return `
+    return  `
     <!DOCTYPE html>
     <html lang="en">
   
@@ -18,7 +41,7 @@ function generateHTML(data) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Team_Profile_Generator/title>
+      <title>Team_Profile_Generator</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
       <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
@@ -32,7 +55,9 @@ function generateHTML(data) {
         </div>
       </header>
       <main class="container my-5">
-        <p> ${data} , ${data[0]}</p>
+        <div class = "row">
+          ${buildCard(data)}  
+        </div>
       </main>
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -41,5 +66,5 @@ function generateHTML(data) {
     `;
   }
 
-  module.exports = generateHTML;
+  
   
